6e49c73075852a834c3f988ad89bb2214a30f6d8
kuba <Kudaiberdigapyrov@gmail.com>
2023-03-07 16:07:51 +0600

    commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] myint = new int[10];
+        int[] myin23 = {1, 2, 3, 4};
+        int[] kuba = new int[20];
+        int[] suui = {1,2,3,4};
+    }
+}
